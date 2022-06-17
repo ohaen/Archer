b83fcfdab93d6fc48d6fc9f4c84e73a89eff4eea
haein <39735571+ohaen@users.noreply.github.com>
2022-06-17 17:44:27 +0900

    Feat : 스타드박스 생성

--- Archer/Assets/Script/Player/MonsterScarchCollider.cs
+++ Archer/Assets/Script/Player/MonsterScarchCollider.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class MonsterScarchCollider : MonoBehaviour
 {
     private Collider[] _collider = new Collider[15];
+    private int _enemyCount;
     void Start()
     {
 
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-
-    }
-
-
     void Update()
     {
         int _monsterLayerMask = (1 << LayerMask.NameToLayer("Monster"));
         int _wallLayerMask = (1 << LayerMask.NameToLayer("Wall"));
-        Physics.OverlapSphereNonAlloc(transform.position, 10.0f, _collider, _monsterLayerMask);
+        _enemyCount = Physics.OverlapSphereNonAlloc(transform.position, 10.0f, _collider, _monsterLayerMask);
+
+        NearMonster();
+    }
+
+    void NearMonster()
+    {
+        if (_enemyCount == 0)
+        {
+            return;
+        }
+        GameObject nearMonster = _collider[0].transform.gameObject;
+        for(int i = 1; i < 15; ++i)
+        {
+            if (_collider[i] == null)
+            {
+                break;
+            }
+
+        }
     }
 }
